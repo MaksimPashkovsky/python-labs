587743bcd959964fbd052e6fbda2a88c54755b90
MaksimPashkovsky <maksim_pashkovskiy@mail.ru>
2022-03-21 12:05:21 +0300

    task 2 upd

--- math_server/raw_sql/5.sql
+++ math_server/raw_sql/5.sql
@@ -1,15 +1,19 @@
 select (
-		select count(distinct (operator, number1, number2))
-		from history
-	) as unique_count,
+		select count(*) from (
+			select operator, number1, number2 from history
+			except all
+			select distinct operator, number1, number2 from history
+		) as fff
+	) as duplicated_count,
 	(
 		select count(*) from (
-			select operator, number1, number2
-			from history
-			except all
-			select distinct operator, number1, number2
-			from history
-		) as fff
-	) as duplicated_count
+			select distinct operator, number1, number2 from history
+			except all (
+				select operator, number1, number2 from history
+				except all
+				select distinct operator, number1, number2 from history
+			)
+		) as ggg
+	) as unique_count
 from history
 group by unique_count
